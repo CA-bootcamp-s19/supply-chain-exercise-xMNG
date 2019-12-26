308a6fcee1b5bd71dad7a85bfbe3a47eb1671dfd
MNG <17662533+xMNG@users.noreply.github.com>
2019-12-26 17:55:51 -0500

    where did my code go?

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,10 +12,12 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint public skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping(uint => Item) items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -24,6 +26,7 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
+  enum State {ForSale, Sold, Shipped, Received}
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
@@ -31,12 +34,28 @@ contract SupplyChain {
     if you need help you can ask around :)
     Be sure to add "payable" to addresses that will be handling value transfer
   */
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
     Prefix each event with "Log" for clarity, so the forSale event will be called "LogForSale"
     Each event should accept one argument, the sku */
+    event LogForSale(uint indexed sku);
+    event LogSold(uint indexed sku);
+    event LogShipped(uint indexed sku);
+    event LogReceived(uint indexed sku);
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
+  modifier onlyOwner() {
+    require(msg.sender == owner, "msg.sender must be owner!");
+    _;
+  }
 
   modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
 
@@ -56,15 +75,28 @@ contract SupplyChain {
    so checking that Item.State == ForSale is not sufficient to check that an Item is for sale.
    Hint: What item properties will be non-zero when an Item has been added?
    */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale(uint sku) {
+    require(items[sku].price != 0 && items[sku].state == State.ForSale, "item not for sale");
+    _;
+  }
+  modifier sold(uint sku) {
+     require(items[sku].state == State.Sold, "item not sold");
+    _;
+  }
+  modifier shipped(uint sku) {
+    require(items[sku].state == State.Shipped, "item not shipped");
+    _;
+  }
+  modifier received(uint sku) {
+    require(items[sku].state == State.Received, "item not received");
+    _;
+  }
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+    owner = msg.sender;
   }
 
   function addItem(string memory _name, uint _price) public returns(bool){
@@ -82,7 +114,13 @@ contract SupplyChain {
 
   function buyItem(uint sku)
     public
-  {}
+    payable
+    forSale(sku)
+    paidEnough(items[sku].price)
+    checkValue(sku)
+  {
+    
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
